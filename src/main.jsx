8f0e7e6ba7843787e353863d74cee3140cb454de
carratuphoenixsrls-design <carratu.phoenixsrls@gmail.com>
2026-07-30 13:26:06 +0200

    style(asset-detail): load compact scoped layout

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import "./Sicurezza8108Page.css";
 import "./components/ProcessEnginePage.css";
 import "./components/masterdata/CanonicalSelect.css";
 import "./FmedUnifiedVisualSystem.css";
+import "./AssetDetailCompact.css";
 
 const FMED_APP_CACHE_VERSION = "fmed-rev0-complete-reconstruction-20260730-3";
 
